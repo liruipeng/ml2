dda0fdb5336047551d7d2d411f81aab18dbbb022
Rui Peng Li <li50@llnl.gov>
2025-05-14 17:13:11 -0700

    add more comments in gs_1d

--- pinn/gs_1d.py
+++ pinn/gs_1d.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import sys
 
 
+# 1D model problem: -u_xx = f
+# with homogeneous boundary conditions (BC).
+
 # Source term
 def f(x, k):
     return k * k * np.pi * np.pi * np.sin(k * np.pi * x)
@@ -33,31 +36,32 @@ def gen_linsys(nx, dx, u, f_arr):
 
 
 def main():
-    maxiter = 200
-    check_freq = 20
+    maxiter = 8
+    check_freq = 2
     ncheck = maxiter // check_freq + 1
     # Grid parameters.
-    nx = 2**8 + 1  # number of POINTS in the x direction
-    k = nx // 2
+    nx = 2**7 + 1  # number of POINTS in the x direction
+    k = nx // 4
     xmin, xmax = 0.0, 1.0  # limits in the x direction
     lx = xmax - xmin  # domain length in the x direction
     dx = lx / (nx - 1)  # grid spacing in the x direction
     x = np.linspace(xmin, xmax, nx)
+    # analytical solution is high-freq + low-freq
     u_analytic = u_ex(x, k) + 10 * u_ex(x, 1)
     f_arr = f(x, k) + 10 * f(x, 1)
     # generate linear system and apply dirichlet BC
     A, f_arr = gen_linsys(nx, dx, u_analytic, f_arr)
+    # exact numerical solution
     u_exact = scipy.linalg.solve(A, f_arr)
     print(f"Discretization Error: {np.linalg.norm(u_exact-u_analytic, ord=np.inf):.4e}")
     fig, ax = plt.subplots(1)
     ax.plot(x, u_analytic, marker='o')
     ax.plot(x, u_exact, 'r', alpha=0.2)
-    #
+    ax.set_title("analytical and numerical solutions of -u_{xx}=f")
+
+    # Gauss-Seidel iterations
     L = np.tril(A)
     u = np.zeros(nx)
-    # u = np.linspace(xmin, xmax, nx)
-    # u = np.random.uniform(0, 1, nx)
-
     fig, ax = plt.subplots(ncheck + 1)
     fig2, ax2 = plt.subplots(ncheck)
     ax[0].plot(x, u_analytic, linestyle='-')
@@ -78,6 +82,9 @@ def main():
         ax[i].get_xaxis().set_visible(False)
     for i in range(len(ax2)-1):
         ax2[i].get_xaxis().set_visible(False)
+
+    ax[0].set_title("iterative solution: u")
+    ax2[0].set_title("error: u_exact - u")
 
     return 0
 
