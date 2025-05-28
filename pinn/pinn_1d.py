d8f2d51779fdbb15a48042dcff140316b9d2d8fd
Rui Peng Li <li50@llnl.gov>
2025-05-27 21:55:14 -0700

    update mlev training

--- pinn/pinn_1d.py
+++ pinn/pinn_1d.py
@@ -14,17 +14,33 @@
 # jupytext --set-formats ipynb,py pinn_1d.py
 # jupytext --sync pinn_1d.py
 
+# %% [markdown]
+# 1D model to solve the problem:
+#
+# $-u_{xx} + \gamma u = f$
+#
+# and homogeneous boundary conditions (BC)
+#
+# #### Problem 1
+# The analytical solution is
+#
+# $u(x) = \sum_k c_k  \sin(w_k  \pi  x)$
+#
+# and
+#
+# $f = \sum_k c_k  (w_k^2  \pi^2 + \gamma)  \sin(w_k  \pi  x)$
+#
+# #### Problem 2 (from [MscaleDNN](https://arxiv.org/abs/2007.11207))
+# The analytical solution is
+#
+# $u(x) = e^{-x^2} \sin(\mu x)$
+#
+# and
+#
+# $f(x) = e^{-x^2} [(r + 4 \mu^2 x^2 - 4 x^2 + 2) \sin(\mu x^2) + (8 \mu x^2 - 2 μ) \cos(\mu x^2)]$
+
 # %%
 # Define modules and device
-"""
-1D PINN model to solve the problem:
-            -u_xx + r*u = f
-and homogeneous boundary conditions (BC).
-The analytical solution is
-   u(x) = sum_k c_k * sin(w_k * pi * x)
-and
-   f = sum_k c_k * (w_k^2 * pi^2 + r) * sin(w_k * pi * x).
-"""
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -39,25 +55,42 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # %%
 # Define PDE
 class PDE:
-    def __init__(self, w=None, c=None, r=0):
+    def __init__(self, w=None, c=None, mu=70, r=0):
         self.w = w if w is not None else [1]
         self.c = c if c is not None else [1]
+        self.mu = mu
         self.r = r
 
     # Source term
-    def f(self, x):
+    def f_1(self, x):
         y = torch.zeros_like(x)
         for w, c in zip(self.w, self.c):
             pi_w = torch.pi * w
             y += c * (pi_w ** 2 + self.r) * torch.sin(pi_w * x)
         return y
 
+    def f_2(self, x):
+        z = x ** 2
+        a = self.r + 4 * z * (self.mu ** 2 - 1) + 2
+        b = self.mu * z
+        c = 8 * b - 2 * self.mu
+        return torch.exp(-z) * (a * torch.sin(b) + c * torch.cos(b))
+
     # Analytical solution
-    def u_ex(self, x):
+    def u_ex_1(self, x):
         y = torch.zeros_like(x)
         for w, c in zip(self.w, self.c):
             y += c * torch.sin(w * torch.pi * x)
         return y
+
+    def u_ex_2(self, x):
+        return torch.exp(-x**2) * torch.sin(self.mu * x ** 2)
+
+    def f(self, x):
+        return self.f_1(x)
+
+    def u_ex(self, x):
+        return self.u_ex_1(x)
 
 # %%
 # Define mesh
@@ -139,6 +172,11 @@ class MultiLevelNN(nn.Module):
 
         # Scale factor
         self.scales = [1.0] * num_levels
+
+    def get_status(self, level_idx: int):
+        if level_idx < 0 or level_idx >= self.num_levels():
+            raise IndexError(f"Level index {level_idx} is out of range")
+        return self.level_status[level_idx]
 
     def set_status(self, level_idx: int, status: LevelStatus):
         assert isinstance(status, LevelStatus), f"Invalid status: {status}"
@@ -261,7 +299,7 @@ class Loss:
 
 # %%
 # Define the training loop
-def train(model, mesh, criterion, iterations, learning_rate, num_check, num_plots, axs1, axs2, level_idx):
+def train(model, mesh, criterion, iterations, learning_rate, num_check, num_plots, axs1, axs2, level_idx, plot=False):
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = StepLR(optimizer, step_size=5000, gamma=0.1)
     check_freq = (iterations + num_check - 1) // num_check
@@ -270,8 +308,9 @@ def train(model, mesh, criterion, iterations, learning_rate, num_check, num_plot
     def to_np(t): return t.detach().cpu().numpy()
 
     u_analytic = mesh.pde.u_ex(mesh.x_eval)
-    axs1[0, level_idx].plot(to_np(mesh.x_eval), to_np(u_analytic), linestyle='-', color="black")
-    axs1[-1, level_idx].plot(to_np(mesh.x_eval), to_np(u_analytic), linestyle='-', color="black", alpha=0.5)
+    if plot:
+        axs1[0, level_idx].plot(to_np(mesh.x_eval), to_np(u_analytic), linestyle='-', color="black")
+        axs1[-1, level_idx].plot(to_np(mesh.x_eval), to_np(u_analytic), linestyle='-', color="black", alpha=0.5)
     ax_i = 1
 
     for i in range(iterations):
@@ -304,9 +343,10 @@ def train(model, mesh, criterion, iterations, learning_rate, num_check, num_plot
                 u_eval = model.get_solution(mesh.x_eval)[:, 0].unsqueeze(-1)
                 error = u_analytic - u_eval.to(u_analytic.device)
                 # plot
-                axs1[ax_i, level_idx].scatter(to_np(mesh.x_train), to_np(u_train), color="red", label="Sample training points")
-                axs1[ax_i, level_idx].plot(to_np(mesh.x_eval), to_np(u_eval), linestyle='-', marker=',', alpha=0.5)
-                axs2[ax_i - 1, level_idx].plot(to_np(mesh.x_eval), to_np(error))
+                if plot:
+                    axs1[ax_i, level_idx].scatter(to_np(mesh.x_train), to_np(u_train), color="red", label="Sample training points")
+                    axs1[ax_i, level_idx].plot(to_np(mesh.x_eval), to_np(u_eval), linestyle='-', marker=',', alpha=0.5)
+                    axs2[ax_i - 1, level_idx].plot(to_np(mesh.x_eval), to_np(error))
                 ax_i += 1
             model.train()
 
@@ -317,15 +357,17 @@ def main():
     eval_resolution = 256
     # Generate training data
     # number of point for training
+    plot = False
     nx = 128
     num_check = 20
     num_plots = 4
-    iterations = 10000
+    iterations = 2000
+    sweeps = 10
     # Domain is interval [ax, bx] along the x-axis
-    ax = 0.0
+    ax = 0
     bx = 1.0
     # PDE coeff
-    h = 32 # highest frequency
+    h = 16 # highest frequency
     w = [h]
     # w = list(range(1, h + 1))
     # w = list(range(2, h + 1, 2))
@@ -341,7 +383,7 @@ def main():
     loss = Loss(loss_type=loss_type)
     print(f"Using loss: {loss.name}")
     # number of levels
-    num_levels = 2
+    num_levels = 4
 
     # ** RELU does NOT work! **
     act = nn.Tanh() # SiLU, Tanh, SoftPlus, GELU
@@ -353,39 +395,48 @@ def main():
                          dim_hidden=[64, 64], act=act)
     print(model)
     model.to(device)
-    # Plotting
-    fig1, axs1 = plt.subplots(num_plots + 1, num_levels)
-    axs1 = axs1.reshape(num_plots + 1, num_levels)
-    fig2, axs2 = plt.subplots(num_plots, num_levels)
-    axs2 = axs2.reshape(num_plots, num_levels)
-    fig1.suptitle("Model Outputs")
-    fig2.suptitle("Error Plots")
     # Train the model
-    for l in range(num_levels):
-        # Turn level l-1 to "frozen"
-        if l > 0:
-            model.set_status(level_idx=l-1, status=LevelStatus.FROZEN)
-        # Turn level l to "train"
-        model.set_status(level_idx=l, status=LevelStatus.TRAIN)
-        print("\nTraining Level", l)
-        model.print_status()
-        # set scale
-        if l == 0:
-            model.set_scale(level_idx=l, scale=1.0)
+    for i in range(sweeps):
+        print("\nTraining Sweep", i)
+        # Plotting
+        if i == sweeps - 1:
+            plot = True
+        if plot:
+            fig1, axs1 = plt.subplots(num_plots + 1, num_levels)
+            axs1 = axs1.reshape(num_plots + 1, num_levels)
+            fig2, axs2 = plt.subplots(num_plots, num_levels)
+            axs2 = axs2.reshape(num_plots, num_levels)
+            fig1.suptitle("Model Outputs")
+            fig2.suptitle("Error Plots")
         else:
-            model.set_scale(level_idx=l, scale=32.0)
-        # Crank that !@#$ up
-        train(model=model, mesh=mesh, criterion=loss, iterations=iterations, learning_rate=1e-3,
-              num_check=num_check, num_plots=num_plots, axs1=axs1, axs2=axs2, level_idx=l)
-    # Plotting
-    for axis_row in axs1[:-1]:
-        for axis in axis_row:
-            axis.get_xaxis().set_visible(False)
-    for axis_row in axs2[:-1]:
-        for axis in axis_row:
-            axis.get_xaxis().set_visible(False)
-    fig1.tight_layout()
-    fig2.tight_layout()
+            axs1 = None
+            axs2 = None
+        # train each level at a time
+        for l in range(num_levels):
+            # Turn all levels to "frozen" if they are not off
+            for k in range(num_levels):
+                if model.get_status(level_idx=k) != LevelStatus.OFF:
+                    model.set_status(level_idx=k, status=LevelStatus.FROZEN)
+            # Turn level l to "train"
+            model.set_status(level_idx=l, status=LevelStatus.TRAIN)
+            print("\nTraining Level", l)
+            model.print_status()
+            # set scale
+            scale = l + 1
+            model.set_scale(level_idx=l, scale=scale)
+            # Crank that !@#$ up
+            train(model=model, mesh=mesh, criterion=loss, iterations=iterations, learning_rate=1e-3,
+                  num_check=num_check, num_plots=num_plots, axs1=axs1, axs2=axs2, level_idx=l, plot=plot)
+        # Plotting
+        if plot:
+            for axis_row in axs1[:-1]:
+                for axis in axis_row:
+                    axis.get_xaxis().set_visible(False)
+            for axis_row in axs2[:-1]:
+                for axis in axis_row:
+                    axis.get_xaxis().set_visible(False)
+            fig1.tight_layout()
+            fig2.tight_layout()
 
     return 0
 
