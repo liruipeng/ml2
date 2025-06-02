84c2d702cfe8bbf01b6c65b0a06bc767abdc2a85
Shao-Ting Chiu <stchiu@email.tamu.edu>
2025-06-02 18:09:40 -0500

    fix: use linear scale BC enforcement

--- pinn/pinn_1d.py
+++ pinn/pinn_1d.py
@@ -259,7 +259,7 @@ class MultiLevelNN(nn.Module):
             y = g0 * (1 - x) + g1 * x + x * (1 - x) * y
             #y = g0 * (1 - x) + g1 * x + (1 - torch.exp(x)) * (1 - torch.exp(x-1)) * y
             #y = g0 + (x-0)/(1-0)*(g1 - g0) + (1 - torch.exp(0-x)) * (1 - torch.exp(x-1)) * y # in domain x in [0, 1]
-            y = (1 - torch.exp(0-x)) * (1 - torch.exp(x-1)) * y 
+            #y = (1 - torch.exp(0-x)) * (1 - torch.exp(x-1)) * y 
         return y
 
     # def _init_weights(self, m):
