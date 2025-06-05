b4b4ef88f0f1883a4d43523776fce69d02b0d3ba
Shao-Ting Chiu <stchiu@email.tamu.edu>
2025-06-04 23:26:23 -0500

    add soap to path

--- pinn/pinn_1d.py
+++ pinn/pinn_1d.py
@@ -56,7 +56,10 @@ from torch.optim.lr_scheduler import StepLR
 import numpy as np
 from enum import Enum
 from utils import parse_args, get_activation, print_args, save_frame, make_video_from_frames, is_notebook, cleanfiles
-from SOAP.soap import SOAP
+import sys 
+dir_pth = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(dir_pth, "SOAP"))
+from soap import SOAP
 
 # torch.set_default_dtype(torch.float64)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
